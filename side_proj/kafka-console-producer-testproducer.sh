898c61302511b5659922cd6779b052f8cad38217
Mathias Olsson <mathias.olsson@westcode.se>
2019-04-03 18:14:46 +0000

    added missing /

--- side_proj/kafka-console-producer-testproducer.sh
+++ side_proj/kafka-console-producer-testproducer.sh
@@ -17,5 +17,5 @@
 if [ "x$KAFKA_HEAP_OPTS" = "x" ]; then
     export KAFKA_HEAP_OPTS="-Xmx512M"
 fi
-exec $(dirname $0)/kafka-run-class.sh -Djava.security.auth.login.config=$(dirname $0)../config/kafka-testproducer-jaas.conf kafka.tools.ConsoleProducer "$@"
+exec $(dirname $0)/kafka-run-class.sh -Djava.security.auth.login.config=$(dirname $0)/../config/kafka-testproducer-jaas.conf kafka.tools.ConsoleProducer "$@"
 #exec $(dirname $0)/kafka-run-class.sh kafka.tools.ConsoleProducer "$@"
